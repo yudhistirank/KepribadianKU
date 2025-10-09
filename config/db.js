b26650e59c6ee3ee507b4ba081ce82d578074a34
yudhistirank <yudhistiranandakumala@gmail.com>
2025-10-09 17:44:32 +0700

    fix vercel.json

--- config/db.js
+++ config/db.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected!");
   } catch (err) {
-    console.error("❌ MongoDB Connection Failed:", err.message);
+    console.error("MongoDB Connection Failed:", err.message);
     process.exit(1);
   }
 };
